ef00fbcd2360120421acabc6ed09d666c1e29799
Willian Roque <willianfernandoroque@gmail.com>
2020-07-21 16:24:44 +0200

    try fixing crash

--- api.cpp
+++ api.cpp
@@ -10,12 +10,13 @@
 // Do some computations with 'str', return the result.
 // This function contains a bug. Can you spot it?
 size_t DoStuff(const std::string &str) {
-  std::vector<int> Vec({0, 1, 2});
-  size_t Idx = 0;
-  if (str.size() > 5) Idx++;
-  if (str.find("foo") != std::string::npos) Idx++;
-  if (str.find("bar") != std::string::npos) Idx++;
-  return Vec[Idx];
+//   std::vector<int> Vec({0, 1, 2});
+//   size_t Idx = 0;
+//   if (str.size() > 5) Idx++;
+//   if (str.find("foo") != std::string::npos) Idx++;
+//   if (str.find("bar") != std::string::npos) Idx++;
+//   return Vec[Idx];
+  return 0;
 }
 
 size_t DoStuffWithMyStruct(struct MyStruct m) {
